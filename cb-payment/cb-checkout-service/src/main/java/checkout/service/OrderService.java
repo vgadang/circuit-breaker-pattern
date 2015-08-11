ab819bf0ffe256e1895796f7a8ebc9726618a383
Vikram Gadang <vikram.gadang@teamaol.com>
2015-08-11 15:22:04 -0400

    Remove binary files

--- cb-payment/cb-checkout-service/src/main/java/checkout/service/OrderService.java
+++
@@ -1,5 +0,0 @@
-package checkout.service;
-
-public interface OrderService {
-	Order purchaseOrder();
-}
--- cb-payment/cb-checkout-service/src/main/java/checkout/service/Payment.java
+++
@@ -1,9 +0,0 @@
-package checkout.service;
-
-public class Payment {
-
-    private long id = 123456L;
-    public long getId() {
-        return id;
-    }
-}
